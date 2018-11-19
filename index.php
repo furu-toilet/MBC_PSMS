df8d51fdedd8ac0c662655fafac64f12e16d2e25
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2018-11-19 16:26:20 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,3 @@
-<?php
-$result = null;
-$msg = null;
-
-if(isset($_POST['sql_submit'])){    //実行ボタン後の処理
-	require "./php/Common.php";
-	$db = new Common();
-	$sql = $_POST['sql'];
-	$result = $db->db_sql($sql);   //SQL実行（戻り値あり）
-	
-	
-	$db->db_close();          //接続切断
-}
-
-if(isset($_POST['sql_reset'])){     //リセットボタン後の処理
-  
-  
-}
-
-function h($str){                   //HTMLに文字列出力
-	return htmlspecialchars($str,ENT_QUOTES,'UTF-8');
-}
-
-
-
-?>
-
 <!DOCTYPE html>
 <html>
   	<head>
@@ -49,33 +22,36 @@ function h($str){                   //HTMLに文字列出力
 		</form>
       <div id="log">
         <h2>実行結果</h2>
-        <!--textarea class="log" rows=20 cols=60-->
-<?php if(!empty($_POST['sql'])){           //SQL実行した場合?>       
-          <?=h('実行SQL：' . $_POST['sql'])?>	
-	  </br>
-    <?php if($db->db_msg() == null){       //エラー時の表示を制御  ?>
-	    <?=h('正常終了') ?></br>
-            <table>
-            <tr>
-        <?php foreach($result[0] as $key => $_): ?>
-            <th><?=h($key) ?></th>
-        <?php endforeach; ?>
-            </tr>
-        <?php foreach($result as $values): ?>
-            <tr>
-                <?php foreach($values as $value): ?>
-                    <td><?=h($value)?></td>
-                <?php endforeach; ?>
-            </tr>
-        <?php endforeach; ?>
-            </table>
+	<script type="text/javascript">
 
-    <?php }else{  //エラーメッセージ ?>	  
-          <?= h($db->db_msg()) ?>	     
-    <?php }   //else end  ?>
-          
-<?php } ?>          
-        <!--/textarea-->
+        $(function(){
+            // Ajax button click
+            $('#ajax').on('click',function(){
+                $.ajax({
+                    url:'./result.php',
+                    type:'POST',
+                    data:{
+                        'sql':$('#sql').val(),
+                    }
+                })
+                // Ajaxリクエストが成功した時発動
+                .done( (data) => {
+                    $('.result').html(data);
+                    console.log(data);
+                })
+                // Ajaxリクエストが失敗した時発動
+                .fail( (data) => {
+                    $('.result').html(data);
+                    console.log(data);
+                })
+                // Ajaxリクエストが成功・失敗どちらでも発動
+                .always( (data) => {
+
+                });
+            });
+        });
+
+    </script>
 		</div>
 	</div>
 </body>
