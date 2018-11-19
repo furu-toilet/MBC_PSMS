db4f6a5cfdb9c42b75e066890d629664c496f7be
furu-toilet <43777130+furu-toilet@users.noreply.github.com>
2018-11-19 16:32:28 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
+<?php
+$result = null;
+$msg = null;
+
+if(isset($_POST['sql_submit'])){    //実行ボタン後の処理
+	require "./php/Common.php";
+	$db = new Common();
+	$sql = $_POST['sql'];
+	$result = $db->db_sql($sql);   //SQL実行（戻り値あり）
+	
+	
+	$db->db_close();          //接続切断
+}
+
+if(isset($_POST['sql_reset'])){     //リセットボタン後の処理
+  
+  
+}
+
+function h($str){                   //HTMLに文字列出力
+	return htmlspecialchars($str,ENT_QUOTES,'UTF-8');
+}
+
+
+
+?>
+
 <!DOCTYPE html>
 <html>
   	<head>
@@ -20,39 +47,35 @@
 			<input class="right-in" type="reset" value="リセット" name="sql_reset" id="sql_reset">
 		</div>
 		</form>
-		<button id="ajax">ajax</button>
       <div id="log">
         <h2>実行結果</h2>
-	<script type="text/javascript">
+        <!--textarea class="log" rows=20 cols=60-->
+<?php if(!empty($_POST['sql'])){           //SQL実行した場合?>       
+          <?=h('実行SQL：' . $_POST['sql'])?>	
+	  </br>
+    <?php if($db->db_msg() == null){       //エラー時の表示を制御  ?>
+	    <?=h('正常終了') ?></br>
+            <table>
+            <tr>
+        <?php foreach($result[0] as $key => $_): ?>
+            <th><?=h($key) ?></th>
+        <?php endforeach; ?>
+            </tr>
+        <?php foreach($result as $values): ?>
+            <tr>
+                <?php foreach($values as $value): ?>
+                    <td><?=h($value)?></td>
+                <?php endforeach; ?>
+            </tr>
+        <?php endforeach; ?>
+            </table>
 
-        $(function(){
-            // Ajax button click
-            $('#ajax').on('click',function(){
-                $.ajax({
-                    url:'./result.php',
-                    type:'POST',
-                    data:{
-                        'sql':$('#sql').val(),
-                    }
-                })
-                // Ajaxリクエストが成功した時発動
-                .done( (data) => {
-                    $('.result').php(data);
-                    console.log(data);
-                })
-                // Ajaxリクエストが失敗した時発動
-                .fail( (data) => {
-                    $('.result').php(data);
-                    console.log(data);
-                })
-                // Ajaxリクエストが成功・失敗どちらでも発動
-                .always( (data) => {
-
-                });
-            });
-        });
-
-    </script>
+    <?php }else{  //エラーメッセージ ?>	  
+          <?= h($db->db_msg()) ?>	     
+    <?php }   //else end  ?>
+          
+<?php } ?>          
+        <!--/textarea-->
 		</div>
 	</div>
 </body>
